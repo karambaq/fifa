658779570756276fc74221750c4591cd65a9d1d6
karambaq <0maximq@gmail.com>
2018-04-28 16:19:11 +0300

    damn.

--- src/hist.py
+++ src/hist.py
@@ -6,7 +6,7 @@ def check_is_end(league, teams, score):
     url = requests.get('http://1xstavka.ru').url + '/getTranslate/ViewGameResultsGroup'
     headers = {'content-type': 'application/json'}
     data = '{"Language":"ru"}{"Params":["%s", null, 85, null, null, 180]}{"Vers":6}{"Adult": false}' % datetime.today().isoformat()[:10]
-    teams = teams.split()
+    teams = [w.strip() for w in teams.split('—')]
     
     response = json.loads(requests.post(url, data=data, headers=headers).text)
     
@@ -14,9 +14,9 @@ def check_is_end(league, teams, score):
         last_couple = l.get('Elems')[-10:]
         for g in last_couple:
             league_str, teams_str = g.get('Head')[4:6]
-            teams_str = teams_str.split()
+            teams_str = [w.strip() for w in teams_str.split('-')]
             score_str = [int(g.get('Head')[6][0]), int(g.get('Head')[6][2])]
-            if [league_str, teams_str[0], teams_str[2]] == [league, teams[0], teams[2]]:
+            if [league_str, teams_str[0], teams_str[1]] == [league, teams[0], teams[1]]:
                 return True
     print('Checking end for %s' % teams)
     return False
